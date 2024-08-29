d00caf6b72ba43ed002c71a6c9d4ab7440898115
HAMID IZZUDIN AL FITROH <hamidizzudinalfitroh04@gmail.com>
2024-08-29 15:49:50 +0700

    mengganti bounce ke fadeInRight

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app" class="container mt-5">
     <h1>IDShop</h1>
-    <p class="animate__animated animate__bounce">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, doloremque, corporis quod, autem cumque in dignissimos nostrum rem quas natus dolorem officia. Eligendi, commodi esse error sint ducimus sapiente vero?</p>
+    <p class="animate__animated animate__fadeInRight">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, doloremque, corporis quod, autem cumque in dignissimos nostrum rem quas natus dolorem officia. Eligendi, commodi esse error sint ducimus sapiente vero?</p>
     <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
   </div>
 </template>
